74405421c2fb3590b7e832e0537cc28355416792
Evan Schlesinger <esslushy@gmail.com>
2022-02-19 23:35:27 -0500

    switched to using numpy

--- Perceptron.py
+++ Perceptron.py
@@ -1,5 +1,5 @@
-from random import randint
 import numpy as np
+import random
 
 class Perceptron:
     def __init__(self, classes=None, metrics={}):
@@ -12,11 +12,13 @@ class Perceptron:
             metrics: what metrics to judge
         """
         # How to interpret the labels
+        if classes and len(classes) != 2:
+            raise Exception('Classes must be an array of length 2')
         self.classes = classes
         # Store metrics that evaluate model
         self.metrics = metrics
         # Initialize weight variable
-        self.w = None
+        self.w = np.array([])
 
     def train_step(self, data, labels):
         """
@@ -26,29 +28,25 @@ class Perceptron:
             data: the data to train on
             labels: the labels for the data
         """
-        if not self.w:
-            self.w = list([randint(-5, 5) for _ in range(len(data[0]) + 1)])
+        # Check if we need to initialize
+        if self.w.size == 0:
+            # One more to account for bias point
+            self.w = np.random.random(len(data[0]) + 1)
         # Get predictions on data
         predictions = self.predict(data)
-        # Gather all the predictions that don't match the actual
-        mislabled = []
-        for i in range(len(predictions)):
-            if predictions[i] != labels[i]:
-                # If they don't match, add the data, prediction, and real label to our mislabled list
-                # Add bias field to data as this is needed when updating weights later
-                mislabled.append({
-                    'data': [1] + data[i], 
-                    'prediction': predictions[i], 
-                    'label': labels[i]
-                    })
-        # Update Weights by a random mislabled point
-        mislabled_point = randint(0, len(mislabled)-1)
-        if mislabled[mislabled_point]['prediction'] == 0:
+        # Find all points with mislabeled
+        mislabled = (predictions != labels)
+        # Get the mislabled data points
+        mislabled_data = data[mislabled]
+        mislabled_label = labels[mislabled]
+        # Select a random data point
+        data_point = random.randint(0, len(mislabled_data) - 1)
+        if mislabled_label[data_point] == 1:
             # Prediction says it is class 0, when we want it to be class 1, meaning x dot w is too small and must be increased
-            self.w = list([self.w[i] + mislabled[mislabled_point]['data'][i] for i in range(len(self.w))])
+            self.w += np.insert(mislabled_data[data_point], 0, 1)
         else:
             # Prediction says it is class 1, we want it to be class 0, meaning x dot w is too big and must be decreased
-            self.w = list([self.w[i] - mislabled[mislabled_point]['data'][i] for i in range(len(self.w))])
+           self.w -= np.insert(mislabled_data[data_point], 0, 1)
         # Return how the model is doing based on passed in parameters
         return {key: self.metrics[key](list(self.predict(data)), labels) for key in self.metrics.keys()}
 
@@ -79,11 +77,14 @@ class Perceptron:
           Args:
             data: the data to make predictions on
         """
-        # Add bias to data
+        # Add bias to data (insert one at the top of each data point)
         data = np.insert(data, 0, 1, axis=1)
-        # Checks if the dot product of the data is positive (class 1)
-        # or negative (class 0). Adds bias vector at the top
-        return np.dot(data, self.w)
+        # Computes the dot product of each piece of data with the perceptron weights
+        predictions = np.dot(data, self.w)
+        # Turn the raw predictions into 0 or 1 class predictions
+        predictions[predictions >= 0] = 1
+        predictions[predictions < 0] = 0
+        return predictions
 
     def predict_classes(self, data):
         """
@@ -94,21 +95,9 @@ class Perceptron:
         """
         if self.classes:
             # Translates class 0 or class 1 to the real labels if provided
-            return [self.classes[pred] for pred in self.predict(data)]
+            classify = np.vectorize(lambda x: self.classes[x])
+            return classify(self.predict(data))
         return self.predict(data)
-        
-
-def dot(a, b):
-    """
-      Computes the dot product of a and b
-
-      Args:
-        a: A vector
-        b: A vector
-    """
-    if len(a) != len(b):
-        raise Exception(f'Vector {a} and Vector {b} are not the same size.')
-    return sum([a[i] * b[i] for i in range(len(a))])
 
 def accuracy(pred, actual):
     """
@@ -118,4 +107,4 @@ def accuracy(pred, actual):
         pred: Predicted class labels
         actual: The actual class labels
     """
-    return sum([int(pred[i] == actual[i]) for i in range(len(actual))])/len(actual)
+    return sum(pred == actual)/len(pred)
